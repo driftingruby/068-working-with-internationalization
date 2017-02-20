256fdfae5f6ab3497bfa654ee68a0915b64d0b76
Dave Kimura <dave@k-innovations.net>
2017-02-19 23:11:51 -0500

    068

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'visitors#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    resources :users
+    root 'visitors#index'
+  end
 end
